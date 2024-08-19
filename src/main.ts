00fd7b681a0bb2fa5d7cbba591ca90ecc30f1d65
mufurkand <mfurkand115@gmail.com>
2024-08-19 16:30:01 +0300

    Override toJSON method of Decimal to return a number instead of a string

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { VALIDATION_PIPE_OPTIONS } from './common/constants/common.constants';
+import { Decimal } from '@prisma/client/runtime/library';
 
 async function bootstrap() {
+  // override the toJSON method of Decimal to return a number instead of a fucking string
+  Decimal.prototype.toJSON = function () {
+    return this.toNumber();
+  };
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe(VALIDATION_PIPE_OPTIONS));
